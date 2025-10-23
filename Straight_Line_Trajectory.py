bfcd07d2c75a3f63023d114a34bd893335266136
Manas Nair <nairwork1209@gmail.com>
2025-10-23 09:40:11 +0530

    Updated_To_SCARA
    Updated trajectory points and inverse kinematics calculations for a SCARA robot. Adjusted plot limits and added labels for better visualization.

--- Straight_Line_Trajectory.py
+++ Straight_Line_Trajectory.py
@@ -1,17 +1,17 @@
 import numpy as np
 import matplotlib
-matplotlib.use('TkAgg') # For pop-up window
+matplotlib.use('TkAgg') 
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import time
 
-# robot parameters and steps
-L1, L2, L3 = 2.0, 5.0, 4.0
+# parameters
+L1, L2 = 5.0, 4.0  
 steps = 50 
 
 # straight line trajectory points
-P_start = np.array([6.0, 2.0, 7.0])
-P_end   = np.array([5.0, 3.0, 6.0])
+P_start = np.array([6.0, 2.0, -1.0]) 
+P_end   = np.array([3.0, 5.0, -4.0]) 
 
 x_traj = np.linspace(P_start[0], P_end[0], steps)
 y_traj = np.linspace(P_start[1], P_end[1], steps)
@@ -22,39 +22,35 @@ plt.ion()
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
-# animation
+# loop
 for i in range(steps):
     Px, Py, Pz = x_traj[i], y_traj[i], z_traj[i]
     
-    # ik
-    t1 = np.arctan2(Py, Px)
-    r = np.sqrt(Px**2 + Py**2)
-    s = Pz - L1
-    
-    
-    cos_t3 = (r**2 + s**2 - L2**2 - L3**2) / (2 * L2 * L3)
-    t3 = -np.arccos(cos_t3) 
-    
-    # Find t2
-    k1 = L2 + L3 * np.cos(t3)
-    k2 = L3 * np.sin(t3)
-    t2 = np.arctan2(r, s) - np.arctan2(k2, k1)
+    # IK
+    # Solve d3 (prismatic joint)
+    d3 = -Pz 
+    cos_t2_num = Px**2 + Py**2 - L1**2 - L2**2
+    cos_t2_den = 2 * L1 * L2
+    if abs(cos_t2_num / cos_t2_den) > 1:
+        print(f"Skipping unreachable point ({Px:.1f}, {Py:.1f})")
+        continue # Skip this step
+        
+    t2 = -np.arccos(cos_t2_num / cos_t2_den) 
+    k1 = L1 + L2 * np.cos(t2)
+    k2 = L2 * np.sin(t2)
+    t1 = np.arctan2(Py, Px) - np.arctan2(k2, k1)
 
-    # fk
-    P0 = np.array([0, 0, 0])
-    P1 = np.array([0, 0, L1])
+    # plot
+    P0 = np.array([0, 0, 0]) 
     
-    P2_x = (L2 * np.sin(t2)) * np.cos(t1)
-    P2_y = (L2 * np.sin(t2)) * np.sin(t1)
-    P2_z = L1 + (L2 * np.cos(t2))
-    P2 = np.array([P2_x, P2_y, P2_z])
-    
+    P1_x = L1 * np.cos(t1)
+    P1_y = L1 * np.sin(t1)
+    P1 = np.array([P1_x, P1_y, 0])
+    P2 = np.array([Px, Py, 0])
     P3 = np.array([Px, Py, Pz]) 
 
     # plot
     ax.cla() 
-    
-    # Plot the full path
     ax.plot(x_traj, y_traj, z_traj, 'c--')
     
     # Plot the arm
@@ -63,8 +59,9 @@ for i in range(steps):
     zs = [P0[2], P1[2], P2[2], P3[2]]
     ax.plot(xs, ys, zs, 'k-', marker='o') 
     
-    # Add one line of plot limits to stop it from jumping
-    ax.set_xlim(-8, 8); ax.set_ylim(-8, 8); ax.set_zlim(0, 11)
+    # Add plot limits for SCARA
+    ax.set_xlim(-10, 10); ax.set_ylim(-10, 10); ax.set_zlim(-5, 5)
+    ax.set_xlabel('X'); ax.set_ylabel('Y'); ax.set_zlabel('Z')
     
     plt.draw()
     time.sleep(0.05)
